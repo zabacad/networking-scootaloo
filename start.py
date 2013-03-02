5e30569e87fbfb52c3146682afb49778ab54ee33
Ian Shearin <zabacad@gmail.com>
2013-03-02 14:36:29 -0800

    Added code to start server and clients.

--- start.py
+++ start.py
@@ -2,11 +2,26 @@
 
 
 import sys
+import Server
+import Client
 
 
-def main():
-	pass
+def start_server(port):
+	server = Server.Server(port)
+
+
+def start_client(port):
+	client = Client.Client(port)
 
 
 if __name__ == '__main__':
-	main();
+	if len(sys.argv) < 3:
+		print "Usage: start server|client port"
+	action = sys.argv[1]
+	port = sys.argv[2]
+	if action == 'server':
+		start_server(port)
+	if action == 'client':
+		start_client(port)
+	else
+		print "Unknown action"
