5a957eb4f5a99b2332f961182f3a0fb1d04fc70c
Ian Shearin <zabacad@gmail.com>
2013-03-02 15:13:39 -0800

    Fixed world class. Made world class printable.

--- World.py
+++ World.py
@@ -4,16 +4,24 @@ class World:
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
-		self.world = [[" " for x in xrange(self.width)] for y in xrange(self.height)]
+		self.world = [[x for x in xrange(self.width)] for y in xrange(self.height)]
 
 	def __del__(self):
 		pass
 
+	def __str__(self):
+		printable = ""
+		for y in xrange(self.height):
+			for x in xrange(self.width):
+				printable += str(self.get(x, y))
+			printable += "\n"
+		return printable
+
 	def get(self, x, y):
-		return self.world[x][y]
+		return self.world[y][x]
 
 	def set(self, x, y, char):
-		self.world[x][y] = char
+		self.world[y][x] = char
 
 	def reset(self, x, y):
 		self.set(x, y, " ")
