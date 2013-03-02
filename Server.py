d55392d6d7cfb9c44b98fc2420b82847f61cf5a5
Ian Shearin <zabacad@gmail.com>
2013-03-02 12:57:36 -0800

    Created basic message-printing server.

--- Server.py
+++ Server.py
@@ -1,10 +1,35 @@
+import socket
 
 
 class Server:
 	"""Server."""
 
 	def __init__(self, port):
-		self.port = port
+		self.addr = ('', port)
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.setblocking(0)
+		self.s.bind(self.addr)
+		self.connections = []
+
+		#self.world
+
+		while True:
+			try:
+				self.s.listen(1)
+				self.connections.append(self.s.accept())
+				print "Got connection"
+			except socket.error:
+				pass
+
+			for conn in self.connections:
+				conn_sock = conn(0)
+				try:
+					data = conn_sock.recv(1024)
+					print data
+				except:
+					pass
 
 	def __del__(self):
-		pass
+		for conn in self.connections:
+			conn.close()
+			"Closed connection (server stop)"
